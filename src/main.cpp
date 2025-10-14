698435419f9dde664c23d3eab2fbcc1e145957b6
NathanKim16 <nathan.kim@ufl.edu>
2025-10-14 02:02:26 -0400

    Basic file setup for Economic Visualizer

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
 //Developed by Nathan Kim, Ashtyn Connoley,
 //and Nicole Wu for Project 2 of COP3503 at 
 //the University of Florida.
+
+
+//Consider using VTK or maybe Tangram. VTK seems to be the best option atm.
